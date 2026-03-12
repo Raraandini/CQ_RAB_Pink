b5143f6faa5d47266fa36f5cd3b177dd2725e473
Raraandinii <raraandinib@gmail.com>
2026-03-12 16:07:55 +0800

    first commit

---
+++ PraktikumADO/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;  
+
+namespace PraktikumADO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHitungMhs_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtHasil_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
