91c45b7fe2a94e2f3a479098e4887e74daeac6ec
Raraandinii <raraandinib@gmail.com>
2026-03-12 16:14:14 +0800

    Variabel untuk menjalankan perintah SQL

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -14,6 +14,8 @@ namespace PraktikumADO
     public partial class Form1 : Form
     {
         SqlConnection conn;
+        SqlCommand cmd;
+
         public Form1()
         {
             InitializeComponent();
